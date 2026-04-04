03999e4952bb132aecefbf078a113ab8b2dd476b
Vinicius Griebler <vinicius-griebler@hotmail.com>
2026-04-04 16:45:54 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -24,9 +24,22 @@ app.get("/movies", async (_, res) => {
 app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
-try{
-    await prisma.movie.create({
-        data: {
+    try {
+
+        //  case insensitive - se a busca for feita por john wick ou John wick ou JOHN WICK, o registro vai ser retornado na consulta
+
+        // case sensitive - se buscar por john wick e no banco estiver como John Wick, não vai ser retornado a consulta
+
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: { equals: title, mode: "insensitive" } },
+        });
+
+        if (movieWithSameTitle) {
+            return res.status(409).send({ message: "Já existe um filme cadastrado com esse titulo" });
+        }
+
+        await prisma.movie.create({
+            data: {
                 title,
                 genre_id,
                 language_id,
@@ -34,13 +47,13 @@ try{
                 release_date: new Date(release_date)
             }
         });
-}catch(error){
-    return res.status(500).send({message: "Falha ao cadastrar um filme"});
-};
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
+    };
 
-res.status(201).send();
+    res.status(201).send();
 });
 
 app.listen(port, () => {
-        console.log(`Servidor rodando em http://localhost:${port}`);
-    });
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
